56ee48452a64265fa15e361ebce23637a7606dae
Adrenos <n.dizier@laposte.net>
2016-08-10 17:16:28 +0200

    gun test

--- Assets/Standard Assets/2D/Scripts/PlatformerCharacter2D.cs
+++ Assets/Standard Assets/2D/Scripts/PlatformerCharacter2D.cs
@@ -21,6 +21,8 @@ namespace UnityStandardAssets._2D
         private Rigidbody2D m_Rigidbody2D;
         private bool m_FacingRight = true;  // For determining which way the player is currently facing.
         private bool test=false;
+        Transform gun;
+
 
         private void Awake()
         {
@@ -29,6 +31,7 @@ namespace UnityStandardAssets._2D
             m_CeilingCheck = transform.Find("CeilingCheck");
             m_Anim = GetComponent<Animator>();
             m_Rigidbody2D = GetComponent<Rigidbody2D>();
+            gun = transform.FindChild("gun");
         }
 
 
@@ -120,6 +123,10 @@ namespace UnityStandardAssets._2D
             Vector3 theScale = transform.localScale;
             theScale.x *= -1;
             transform.localScale = theScale;
+
+            Vector3 theScale2 = gun.localScale;
+            theScale2.x *= -1;
+            gun.localScale = theScale2;
         }
     }
 }
---
+++ Assets/script/gunRotation.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class gunRotation : MonoBehaviour {
+
+    [SerializeField]public int rotationOffset = 90;
+	// Update is called once per frame
+	void Update () {
+
+        float x = Input.mousePosition.x;
+        float y = Input.mousePosition.y;
+
+        Vector3 difference = Camera.main.ScreenToWorldPoint(new Vector3(x, y, 5)) - transform.position;
+        difference.Normalize();
+        float rotZ =Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, rotZ + rotationOffset);
+	}
+}
