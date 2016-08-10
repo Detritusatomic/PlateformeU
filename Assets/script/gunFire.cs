79d9e99efc7ae67d70af56a0c2df225f14504005
Adrenos <n.dizier@laposte.net>
2016-08-11 00:49:08 +0200

    bullets

--- Assets/script/gunFire.cs
+++ Assets/script/gunFire.cs
@@ -7,6 +7,8 @@ public class gunFire : MonoBehaviour {
     [SerializeField]public float damage = 10;
     [SerializeField]public LayerMask notToHit;
 
+    [SerializeField]public Transform bulletPrefab;
+
     private float timeToFire = 0;
     Transform firePoint;
 
@@ -44,10 +46,16 @@ public class gunFire : MonoBehaviour {
         Vector2 mousePosition = new Vector2(Camera.main.ScreenToWorldPoint(new Vector3(x, y, -Camera.main.transform.position.z)).x, Camera.main.ScreenToWorldPoint(new Vector3(x, y, -Camera.main.transform.position.z)).y);
         Vector2 firePointPosition = new Vector2(firePoint.position.x, firePoint.position.y);
         RaycastHit2D hit = Physics2D.Raycast(firePointPosition, mousePosition-firePointPosition,10, notToHit);
+        Effect();
         Debug.DrawLine(firePointPosition, mousePosition, Color.green);
         if(hit.collider != null)
         {
             Debug.DrawLine(firePointPosition, hit.point, Color.red);
         }
+    }
+
+    void Effect()
+    {
+        Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
     }
 }
