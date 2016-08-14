0cf9600a0b5c8f02658f095e36d50924e56dd971
Adrenos <n.dizier@laposte.net>
2016-08-15 00:59:15 +0200

    spike de ice

---
+++ Assets/time.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class time : MonoBehaviour {
+
+
+	
+	// Update is called once per frame
+	void Update () {
+        Time.timeScale = 1;
+	}
+}
